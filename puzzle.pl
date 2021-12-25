c27f942652341777f979a45036f14344159bf056
조태혁 <95hyouka@gmail.com>
2021-12-26 00:37:53 +0900

    왜 함수형 프로그래밍을 하고 있을까

--- puzzle.pl
+++ puzzle.pl
@@ -38,3 +38,23 @@ product_candidates_from_sum(S,OUT):-
 product_candidates_foreach_sum_candidate(X):-
     sum_candidates_for_p(S),
     maplist([Sum,Out]>>product_candidates_from_sum(Sum,Out),S,X).
+
+product_candidates_no_brain(X):-
+    product_candidates_foreach_sum_candidate(L),
+    foldl(union, L, [], Y),
+    sort(Y,X).
+
+p_know_the_answer(P):-
+    product_candidates_foreach_sum_candidate(Sets),
+    foldl([Set,Old,New]>>(contains_term(P,Set) -> New is Old+1 ; New is Old),Sets,0,Result),
+    Result == 1.
+
+% 30,42,60,72,120,126,180
+product_candidates(Ps):-
+    product_candidates_no_brain(L),
+    exclude(p_know_the_answer, L, Ps).
+
+% third statement: S now knows the answer.
+% product_candidates_foreach_sum_candidate(X)
+% X = [[18, 24, 28, 30], [30, 42, 52, 60, 66, 70, 72], [42, 60, 76, 90, 102, 112|...], [50, 72, 92, 110, 126|...], [54, 78, 100, 120|...]]
+% Sum = 11, Product = 30
